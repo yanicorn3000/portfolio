4d602b1087b5b944fb05c616d75db146bec8e4e2
yanicorn3000 <yanadziabiola@gmail.com>
2025-03-17 15:30:46 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/",
+          dest: "",
+        },
+      ],
+    }),
+  ],
   base: "/portfolio/",
+  // build: {
+  //   outDir: "../dist",
+  // },
 });
